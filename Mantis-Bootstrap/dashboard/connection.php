780c38c02963ce29bf90406aaa8e759fb5930874
anji-raychura <anjaliraychura9898@gmail.com>
2025-03-24 09:59:29 +0530

    Initial commit

---
+++ Mantis-Bootstrap/dashboard/connection.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "database";
+
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+if($conn)
+{
+  echo "Connection Done <br>";
+}
+else
+{
+    echo "connection Failed".mysqli_connect_error();
+}
+?>
